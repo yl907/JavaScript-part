8164aacb541cbdc6286ea19675f2a2877725e720
yl907 <yluwane@hotmail.com>
2024-04-21 20:47:17 +0800

    really finished this time, I delete all the old files.

--- 1-JavaScript-first-step/1-what-is-javascript/script1.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("button");
-
-button.addEventListener("click", updateName);
-
-function updateName() {
-  const name = prompt("Enter a new name");
-  button.textContent = `Player 1: ${name}`;
-}
